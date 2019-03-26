b423b3cc6a88fd8ab7b5501bbcb014512d127ef6
junior2355 <43444471+junior2355@users.noreply.github.com>
2019-03-26 09:27:00 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$("button").click(function(){
+    $("#money").text("billionaire");
+   
+});
+$("#god").dblclick(function(){
+    $("#killformoney").text("I will build a mansion");
+});
+$("#skullimg").mouseenter(function(){
+    $("#skull").text("I will have revenge");
+});
+$("").mouseleave(function(){
+    $("").text("");
+});
+
+
+
